77ce7b8db5ffff3d52ff9ef89d45876f9a2f6cd8
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-09-01 14:01:45 +0200

    Update matlab-sheet.m

--- matlab-sheet.m
+++ matlab-sheet.m
@@ -1,76 +1,57 @@
 
-Mat Lab commands
-	clc - clears command window
-	no data types definitions 
-	z(5) if string it returns the character 5 in string z 
-	string is in single quotation
-	clear - clears the variables values
-	clear varName - clears the specified value
-	whos - returns data about all your variables
-	mat lab thinks about matrices
-	z' gives the transpose of the element
-	any field in mat lab takes 8 bytes
-	semi-colon after any command hides the result form screen
+% Use this sybol for comments.
+% no data types definitions - matlab is dynamic typing
+% semi-colon after any command hides the result form screen
 
-	file of code
-	write commands in notepad , then copy-past to mat lab
-	new mat lab file >> script
-	file to be done that doesn’t take or return parameters
-	file that take parameters or return it is function
-	the file should be .m meta type
-	then we can edit the file to add commands
-	to execute it just write its name in command
+% general operations
+clear % clears the variables values
+clear varName % clears the specified value
+whos % returns data about all your variables
+clc % clears command window
+pi % mathimatical constant with value 3.14 
 
-	tap to see the anther instr approximation
-	function if we used it only we will get the first arguemnt only
-	[ f , u ]  = add ( 5, 9 )
- 
-	to define matrix 
-	x  =[ 1  5 2 ]
-	x = 1:9
-	from 1 to 9 
-	x = [1:9:2]
-	step 2
-	x = [1:9 , 1:3 ]
-	two periods in our matrix
-	x = [ 2 2 3 ; 1 2 3 ]
-	semi-colon to separate between rows
-	space of comma
-	x .* y is cross product
-	x ./ y is cross division
-	x (: , 1)  targeting the first row in the matrix
-	plot (  x , y , ‘.’)   this dot so keep relation in points only
-	title ‘your name’
-	xlabel ‘ x coordinating names’
+% string operations
+% string is in single quotation
+z(5) % returns the character 5 in string z 
 
-	pi is the 3.14 value
-	-6 : 5. : 6
-	from 6 to 6 with step .5
-	for i = 1:9
-	loop from i = 1 to i = 9
-	won’t be executed until we write end
+% matrix operations
+x  =[ 1  5 2 ] % to define matrix 
+x = 1:9 % from 1 to 9 
+x = [1:9:2] % from 1 to 9 with step 2
+x = [1:9 , 1:3 ] % two periods in our matrix
+x = [ 2 2 3 ; 1 2 3 ] % semi-colon to separate between rows
+x (: , 1) % targeting the first row in the matrix
+z' % transpose of the matrix z with ' sign
+x .* y %  cross product
+x ./ y %  cross division
 
-	in images 
-	the first plane is x (:,:,1 )
+rand() % return random number
+randn() % return positive or negative number
+rand(n) % return n X n random
+ones(n , m ) % return matrix n X m of ones
+zeros(n , m ) % return matrix n X m of zeroes
+
+% script file
+% new mat lab file >> script
+% file that take parameters or return it is function
+% the file should be .m meta type
+% function call example
+[ f , u ]  = add ( 5, 9 )
 
 
-	rand() return random number
-	randn() return positive or negative number
-	rand(n) return n X n random
-	ones(n , m ) return matrix n X m of ones
-	zeros(n , m ) return matrix n X m of zeroes
+% loops
+-6 : 5. : 6
+from 6 to 6 with step .5
+for i = 1:9
+loop from i = 1 to i = 9
 
 
+% plotting matrix 
+plot (  x , y , ‘.’)  % this dot so keep relation in points only
+title ‘your name’
+xlabel ‘ x coordinating names’
 
-	to monitor changes in k-mean
-	opts = statset(‘Display’ , ‘final’) // define structure opts
-	 [idx,ctrs] = kmeans(X,2,   'Distance','city',  'Replicates',5, 'Options',opts)
-	Ctrs will hold coordinates of two centroids  number of classes
-	Idx will hold each point indicator to its group
-
-
-To display the red and blue points related to each class:
-
+% To display the red and blue points related to each class:
 plot(X(idx==1,1),X(idx==1,2),'r.','MarkerSize',12)
 hold on
 plot(X(idx==2,1),X(idx==2,2),'b.','MarkerSize',12)
@@ -81,6 +62,6 @@ plot(ctrs(:,1),ctrs(:,2),'ko',...
 legend('Cluster 1','Cluster 2','Centroids',...
        'Location','NW')
 
-
-
+% in images 
+x (:,:,1 ) % the first plane or RGB planes of the image
 
